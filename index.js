081e58a4ddd315f37dacd4504ae106eee2c27f5e
Sadie Waala <sadie.waala@yahoo.com>
2022-04-15 15:49:15 -0400

    commit tree 75498829e299a2ae6179a81e2faa78d1572c4237

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logShout(string) {
+    console.log('HELLO');
+  }
+  function logWhisper(string) {
+    console.log('hello');
+  }
+  function sayHiToHeadphonedRoommate(string) {
+    console.log(string);
+    console.log(string.toUpperCase());
+  
+    if (string === string.toLowerCase()) {
+      return "I can't hear you!"
+    }
+    if (string === string.toUpperCase()) {
+      return "YES INDEED!"
+    }
+    if (string === "Let's have dinner together!") {
+      return "I would love to!"
+    }
+  }
